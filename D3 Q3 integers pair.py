e031f593f080ad6023439714a33b0e89d74b1560
adikesavareddy0615 <113874228+adikesavareddy0615@users.noreply.github.com>
2022-09-24 11:01:34 +0530

    Add files via upload

--- D3 Q3 integers pair.py
+++ D3 Q3 integers pair.py
@@ -1,14 +1,10 @@
 def printPairs(arr, arr_size, sum):
  hashmap = {}
- 
  for i in range(0, arr_size):
   temp = sum-arr[i]
   if (temp in hashmap):
    print (f'Pair with given sum {sum} is ({temp},{arr[i]}) at indices ({hashmap[temp]},{i})')
   hashmap[arr[i]] = i
-
-
 A = [1, 4, 45, 6, 10, 8]
 n = 16
 printPairs(A, len(A), n)
-
