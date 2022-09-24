3a030be17aca003b1113cc6b254bbcb94257aab9
adikesavareddy0615 <113874228+adikesavareddy0615@users.noreply.github.com>
2022-09-24 11:00:49 +0530

    Add files via upload

---
+++ D5 Q1 reverse.py
@@ -0,0 +1,8 @@
+def rev_sentence(sentence):
+ words = sentence.split(' ')
+ reverse_sentence=""
+ for i in words:
+  reverse_sentence+=i[::-1]+' '
+ return reverse_sentence
+input = "Hello World"
+print (rev_sentence(input))
